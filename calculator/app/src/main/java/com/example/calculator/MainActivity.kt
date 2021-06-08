3c340d533b48036ae5306e5d7712560d42cf90f8
jhlsuper <jhlsuper@gmail.com>
2021-06-08 17:24:51 +0900

    calculator layout done

--- calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setConteentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+    }
+    fun buttonClicked(v: View){
+    }
+    fun resultButtonClicked(v: View){
+    }
+    fun historyButtonClicked(v: View){
+    }
+    fun clearButtonClicked(v: View){
     }
 }
