7718d04ffcb9a2d91defc0792908d68d51c43ae3
jhlsuper <jhlsuper@gmail.com>
2021-07-02 17:57:05 +0900

    making navigatePhotos funtion

--- Digital-Frame/app/src/main/java/com/example/digital_frame/MainActivity.kt
+++ Digital-Frame/app/src/main/java/com/example/digital_frame/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.digital_frame
 
 import android.content.pm.PackageManager
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 
@@ -16,6 +19,17 @@ class MainActivity : AppCompatActivity() {
 
     private val startPhotoFrameModeButton: Button by lazy {
         findViewById<Button>(R.id.btn_startFrameMode)
+    }
+
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.img_1))
+            add(findViewById(R.id.img_2))
+            add(findViewById(R.id.img_3))
+            add(findViewById(R.id.img_21))
+            add(findViewById(R.id.img_22))
+            add(findViewById(R.id.img_23))
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +48,8 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     //권한이 잘 부여되었을때
+                    navigatePhotos()
+
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     showPermissionContextPopup()
@@ -48,6 +64,33 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //권한이 부여가되었다.
+                    navigatePhotos()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셧습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+                else -> {
+                    //
+                }
+            }
+        }
+    private fun navigatePhotos(){
+        
+    }
+
+
+
 
     private fun showPermissionContextPopup() {
         //권한 팝업을 실행하는 함수
