f93f6e4b98657880f8cb785d00fe1d030a7ac2fa
jhlsuper <jhlsuper@gmail.com>
2021-07-05 17:45:37 +0900

    digital frame done little bug

--- Digital-Frame/app/src/main/java/com/example/digital_frame/MainActivity.kt
+++ Digital-Frame/app/src/main/java/com/example/digital_frame/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.digital_frame
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.Image
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -32,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             add(findViewById(R.id.img_23))
         }
     }
-
+    private val imageUriList: MutableList<Uri> = mutableListOf() //초기화까지 같이
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -66,6 +68,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun initStartPhotoFrameModeButton() {
+        startPhotoFrameModeButton.setOnClickListener {
+            val intent = Intent(this, PhotoFrameActivity::class.java)
+            imageUriList.forEachIndexed { index, uri ->
+                intent.putExtra("photo$index",uri.toString())
+            }
+            intent.putExtra("photoListSize",imageUriList.size)
+            startActivity(intent)
+        }
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -81,12 +94,13 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "권한을 거부하셧습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
-                else -> {
-                    //
-                }
+            else -> {
+                //
             }
         }
-    private fun navigatePhotos(){
+    }
+
+    private fun navigatePhotos() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         startActivityForResult(
@@ -95,25 +109,48 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        when (requestCode) {
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+                if (selectedImageUri != null) {
+                    if (imageUriList.size == 6) {
+                        Toast.makeText(this, "이미 사진이 꽉찼습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                // 다른 request일때 지금은 없음
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 
     private fun showPermissionContextPopup() {
         //권한 팝업을 실행하는 함수
         AlertDialog.Builder(this).setTitle("권한이 필요합니다")
-            .setMessage("전자액자 앱에서 사진을 불러오기 위해 권한이 필요합니다")
-            .setPositiveButton("동의하기", { _, _ ->
+            .setMessage("전자액 자 앱에서 사진을 불러오기 위해 권한이 필요합니다")
+            .setPositiveButton("동의하기") { _, _ ->
                 requestPermissions(
                     arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
                     1000
                 )
-            })
+            }
             .setNegativeButton("취소하기", { _, _ -> })
             .create()
             .show()
     }
 
-    private fun initStartPhotoFrameModeButton() {
 
-    }
 }
