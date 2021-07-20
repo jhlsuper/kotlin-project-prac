3528480b22c47003f35b2096bb212ce2f708e7a9
Jeff Lee <jhlsuper@gmail.com>
2021-07-21 00:23:26 +0900

    recorder UI made

---
+++ Voice-Recorder/app/src/main/java/com/example/voicerecorder/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.voicerecorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+
+    //초기 state 설정
+    private var state = State.BEFORE_RECORDING
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+}
