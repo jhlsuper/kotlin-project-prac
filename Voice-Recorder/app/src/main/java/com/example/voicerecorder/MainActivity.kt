77bbf842074b200ec88c536ef2e0bdcd768493cc
Jeff Lee <jhlsuper@gmail.com>
2021-07-21 01:57:21 +0900

    recorder UI and permission request done

--- Voice-Recorder/app/src/main/java/com/example/voicerecorder/MainActivity.kt
+++ Voice-Recorder/app/src/main/java/com/example/voicerecorder/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.voicerecorder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,7 +10,8 @@ class MainActivity : AppCompatActivity() {
     private val recordButton: RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
-
+    //필요한 권한 선언
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     //초기 state 설정
     private var state = State.BEFORE_RECORDING
 
@@ -16,10 +19,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
         initViews()
     }
 
-    fun initViews() {
+    private fun requestAudioPermission(){
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        //권한이 부여받은게 맞는지 check 권한부여받았으면 true 아니면 false
+        val audioRequestPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() ==PackageManager.PERMISSION_GRANTED
+
+        //권한이 부여되지않으면 어플 종료
+        if(!audioRequestPermissionGranted){
+            finish()
+        }
+    }
+
+    private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+    
+    companion object{
+        //permission code 선언
+        private const val REQUEST_RECORD_AUDIO_PERMISSION =201
     }
 }
