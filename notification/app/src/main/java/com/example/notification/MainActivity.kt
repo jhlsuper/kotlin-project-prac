5d7480c650b857570ce23a7f829239593bef8e8d
jhlsuper <jhlsuper@gmail.com>
2021-08-25 17:18:52 +0900

    cloud message 받아오기 성공! / google-services.json은 commit 안함

--- notification/app/src/main/java/com/example/notification/MainActivity.kt
+++ notification/app/src/main/java/com/example/notification/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.notification
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
-    private val resultTextView :TextView by lazy {
+    private val resultTextView: TextView by lazy {
         findViewById(R.id.resultTextView)
     }
 
@@ -17,5 +18,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+    }
+
+    private fun initFirebase(){
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+
+                firebaseToken.text = task.result
+            }
+        }
     }
 }
